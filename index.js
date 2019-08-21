d786628cefcad01d67075e57e216d61c3a864ab4
bennybermuda <laux6459@stanford.edu>
2019-08-20 23:08:26 -0700

    started sticker bulletin

---
+++ index.js
@@ -0,0 +1,11 @@
+var app = require('express')();
+//function handler that you can supply to an HTTP server
+var http = require('http').createServer(app);
+
+app.get('/', function(req, res){
+  res.send('<h1>Hello world</h1>');
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
